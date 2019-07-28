b5034b3ec21c3e0e29b3159a93bc48a83f299895
Pythonsway <adam@pythonsway.it>
2019-07-28 14:36:51 +0200

    PWA changes

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -39,11 +39,11 @@ export function register(config) {
         checkValidServiceWorker(swUrl, config);
 
         // Add some additional logging to localhost, pointing developers to the
-        // service worker/PWA documentation.
+        // service worker/PWA documentation.  https://bit.ly/CRA-PWA
         navigator.serviceWorker.ready.then(() => {
-          console.log(
-            'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://bit.ly/CRA-PWA'
+          alert(
+            `This web app is being served cache-first
+            by a service worker.`
           );
         });
       } else {
@@ -68,10 +68,10 @@ function registerValidSW(swUrl, config) {
             if (navigator.serviceWorker.controller) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
-              // content until all client tabs are closed.
-              console.log(
-                'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+              // content until all client tabs are closed. https://bit.ly/CRA-PWA
+              alert(
+                `New content is available and will be used when
+                all tabs for this page are closed. See`
               );
 
               // Execute callback
@@ -82,7 +82,7 @@ function registerValidSW(swUrl, config) {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
-              console.log('Content is cached for offline use.');
+              alert(`Content is cached for offline use.`);
 
               // Execute callback
               if (config && config.onSuccess) {
@@ -120,8 +120,9 @@ function checkValidServiceWorker(swUrl, config) {
       }
     })
     .catch(() => {
-      console.log(
-        'No internet connection found. App is running in offline mode.'
+      alert(
+        `No internet connection found.
+        App is running in offline mode.`
       );
     });
 }
