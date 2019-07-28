bbadc257ca34b37e64193f26c377ec7ef93be965
Pythonsway <adam@pythonsway.it>
2019-07-28 11:33:17 +0200

    Updates

---
+++ precache-manifest.99c6e9f7c3e0f8921fa9572ca6ffab00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ed684893eca3e2dbc5f25e651b2358",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "2fd4dddc5e4cdf6b5d10",
+    "url": "/drum-machine/static/css/main.e524e640.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/drum-machine/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "2fd4dddc5e4cdf6b5d10",
+    "url": "/drum-machine/static/js/main.f34f403d.chunk.js"
+  },
+  {
+    "revision": "afcf9fe73353caf758cf",
+    "url": "/drum-machine/static/js/runtime~main.27309cc5.js"
+  }
+]);
